62d023a044a64a7e5e398c833a62fd5a065ef02c
Lukas <lukas@passeride.com>
2023-03-08 14:36:52 +0100

    Adding icons

--- world.cs
+++ world.cs
@@ -3,57 +3,72 @@ using System;
 
 public partial class world : Node3D
 {
-	private Camera3D _camera;
-	private RayCast3D _raycast = new RayCast3D();
-	private Agent _agent;
+    [Export]
+    public PackedScene FireScene;
+    private Camera3D _camera;
+    private RayCast3D _raycast = new RayCast3D();
+    private Agent _agent;
 
+    public enum ClickState
+    {
+        NORMAL,
+        PLACE_FIRE,
+        ADD_FIRE,
+        REMOVE_FIRE,
+        MOVE_AGENTS
+    }
 
-	// [Signal]
-	// public delegate void PointClickPositionEventHandler(Vector3 pos);
+    private ClickState _click_state = ClickState.NORMAL;
+
+    // [Signal]
+    // public delegate void PointClickPositionEventHandler(Vector3 pos);
 
     public override void _Ready()
     {
         _camera = GetNode<Camera3D>("Camera3D");
-		_raycast.Name = "MouseRaycast";
-		_camera.AddChild(_raycast);
-		_raycast.Enabled = true;
-		_raycast.ClearExceptions();
+        _raycast.Name = "MouseRaycast";
+        _camera.AddChild(_raycast);
+        _raycast.Enabled = true;
+        _raycast.ClearExceptions();
     }
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
 
-	public void FireDetected()
-	{
-		
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta) { }
+
+    public void SetClickState(ClickState newState)
+    {
+        _click_state = newState;
+    }
+
+    public void FireDetected()
+    {
         // GetTree().CallGroup("Agents", "MoveTo", hit_point);
         GetTree().CallGroup("TV", "setShowsEscapeRoute", true);
-	}
-	public override void _Input(InputEvent @event)
-	{
-        return; // Disable click to summon
-		if (@event is InputEventMouseButton eventMouseButton &&
-			eventMouseButton.Pressed)
-		{
-             Vector3 origin =
-                 _camera.ProjectRayOrigin(eventMouseButton.Position);
-             Vector3 direction =
-                 _camera.ProjectRayNormal(eventMouseButton.Position);
-             Vector3 end = origin + direction * 1000;
-			 PhysicsDirectSpaceState3D state = GetWorld3D().DirectSpaceState;
-			 var query = new PhysicsRayQueryParameters3D();
-			 query.From = origin;
-			 query.To = end;
-			 var intersection = state.IntersectRay(query);
+    }
 
-			 if(intersection.Count > 0){
+    public override void _Input(InputEvent @event)
+    {
+        if (_click_state == ClickState.MOVE_AGENTS)
+        {
+            if (@event is InputEventMouseButton eventMouseButton && eventMouseButton.Pressed)
+            {
+                Vector3 origin = _camera.ProjectRayOrigin(eventMouseButton.Position);
+                Vector3 direction = _camera.ProjectRayNormal(eventMouseButton.Position);
+                Vector3 end = origin + direction * 1000;
+                PhysicsDirectSpaceState3D state = GetWorld3D().DirectSpaceState;
+                var query = new PhysicsRayQueryParameters3D();
+                query.From = origin;
+                query.To = end;
+                var intersection = state.IntersectRay(query);
 
-				 Vector3 hit_point = ((Vector3)intersection["position"]);
-                 GetTree().CallGroup("Agents", "MoveTo", hit_point);
-                 GetTree().CallGroup("TV", "setShowsEscapeRoute", true);
-				 // _agent.MoveTo(hit_point);
-			 }
-		}
-	}
+                if (intersection.Count > 0)
+                {
+                    Vector3 hit_point = ((Vector3)intersection["position"]);
+                    GetTree().CallGroup("Agents", "MoveTo", hit_point);
+                    // GetTree().CallGroup("TV", "setShowsEscapeRoute", true);
+                    // _agent.MoveTo(hit_point);
+                }
+            }
+        }
+    }
 }
